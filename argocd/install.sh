c13022503c6e0f3d9cd940dbe1c504be036793c4
Paul McKee <paul.mckee.aus@gmail.com>
2023-08-05 10:12:23 +1000

    Added AgroCD

--- argocd/install.sh
+++ argocd/install.sh
@@ -9,7 +9,12 @@ kubectl apply -n argocd -f install.yaml
 
 kubectl create -n argocd secret tls argocd-server-tls --cert=`~/argocd/cert.pem`  --key=`~/argocd/key.pem`
 
-
+kubectl create -n argocd secret tls argocd-server-tls \
+  --cert=~/argocd/cert.pem \
+  --key=~/argocd/key.pem
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
+
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
